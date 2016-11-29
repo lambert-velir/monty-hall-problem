b4b5bef07207d8be22cfe19c1d6628bb476c3c2b
Mike Lambert <michael.lambert@velir.com>
2016-11-29 16:01:44 -0500

    Skeleton app

---
+++ app/js/components/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default React.createClass({
+
+    getInitialState() {
+        return {
+        };
+    },
+
+
+    render() {
+
+        return (
+            <div>
+                Monty hall problem!
+            </div>
+        );
+    }
+});
